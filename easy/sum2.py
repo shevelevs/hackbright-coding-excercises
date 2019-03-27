ec869beb59e416b803dc92c848e6077d79bb0023
Sergey Shevelev <sshevelev2@bloomberg.net>
2019-03-26 20:05:25 -0700

    Added easy/sum2.py

---
+++ easy/sum2.py
@@ -0,0 +1,16 @@
+# Given an array of integers, return indices of the two numbers 
+# such that they add up to a specific target.
+#
+# You may assume that each input would have exactly one solution, 
+# and you may not use the same element twice.
+#
+# Example:
+#
+# Given nums = 2, 7, 11, 15, target = 9,
+# 
+# Because nums0 + nums1 = 2 + 7 = 9, return 0, 1.
+
+def sum2(nums):
+    # todo: fill the bpdy of the function
+    pass
+
